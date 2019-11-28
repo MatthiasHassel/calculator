431bb7526ce50a9095b243ca535d9b0a021e7715
Jonas Ohland <jonas.ohland@gmail.com>
2019-11-28 14:09:05 +0100

    initial commit

---
+++ src/calculator.cpp
@@ -0,0 +1,28 @@
+#include <exception>
+#include <iostream>
+#include <string>
+#include <vector>
+
+float calculate(const std::string& lhs,
+                const std::string& rhs,
+                const std::string& operand_string)
+{
+    float flhs = std::stof(lhs);
+    float frhs = std::stof(rhs);
+
+    if (operand_string == "+")
+        return flhs + frhs;
+    else if (operand_string == "-")
+        return flhs - frhs;
+    else
+        return 0;
+}
+
+int main(int argc, const char** argv)
+{
+    ++argv;
+
+    float output = calculate(argv[0], argv[2], argv[1]);
+
+    std::cout << "Ergebnis: " << output << "\n";
+}
